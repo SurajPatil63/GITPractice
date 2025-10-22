51d6a30a9008d474ca9273c6c234187e6a524140
suraj <surajpatil11163@gmail.com>
2025-10-22 23:52:03 +0530

    QA: added the new commit by SRJ

--- hello.py
+++ hello.py
@@ -1,18 +1,26 @@
 def greet(name):
     return f"Hello, {name}!"
 
-if __name__ == "__main__":
-    print(greet("Suraj"))
-
 def add_numbers(a, b):
     return a + b
+
+def minus_numbers(a, b):
+    return a - b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def divide_numbers(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
 
 if __name__ == "__main__":
     print(greet("Suraj"))
     print("Sum of 5 and 3 is:", add_numbers(5, 3))
-
-def minus_numbers(a, b):
-    return a - b
+    print("Difference of 5 and 3 is:", minus_numbers(5, 3))
+    print("Product of 5 and 3 is:", multiply_numbers(5, 3))
+    print("Division of 5 by 3 is:", divide_numbers(5, 3))
 
 if __name__ == "__main__":
     print(greet("Suraj"))
